f64d11c2ad6d88a47e701e024ecbb64629c27195
Gaurav Singh <sgaurav4683@gmail.com>
2024-02-01 22:34:18 +0530

    about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,9 +10,11 @@ import bgImg from "../assets/cyber-bg.png";
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-between bg-zinc-200">
+    <div
+      name="home"
+      className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
+    >
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        {/* left side */}
         <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
           <p className="text-2xl">Unique Sequencing & Production</p>
           <h1 className="py-3 text-5xl md:text-7xl font-bold">
@@ -21,7 +23,6 @@ const Hero = () => {
           <p className="text-2xl">This is our Tech brand.</p>
           <button className="py-3 px-6 sm:w-[60%] my-4">Get Started</button>
         </div>
-        {/* right side */}
         <div>
           <img className="w-full" src={bgImg} alt="/" />
         </div>
@@ -29,6 +30,7 @@ const Hero = () => {
           className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
             border border-slate-300 rounded-xl text-center shadow-xl"
+          style={{ zIndex: 20 }}
         >
           <p>Data Services</p>
           <div className="flex justify-between flex-wrap px-4">
