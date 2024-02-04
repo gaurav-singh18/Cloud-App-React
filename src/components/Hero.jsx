c5451ddb485b8e81497b8480d57bcd9f957a0453
Gaurav Singh <sgaurav4683@gmail.com>
2024-02-04 22:49:48 +0530

    ui changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,24 +26,22 @@ const Hero = () => {
         <div>
           <img className="w-full" src={bgImg} alt="/" />
         </div>
-        <div
-          className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
-            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
-            border border-slate-300 rounded-xl text-center shadow-xl"
-        >
-          <p>Data Services</p>
-          <div className="flex justify-between flex-wrap px-4">
+        <div className="md:min-w-[760px] mx-4 md:mx-auto bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl py-8">
+          <p className="mb-4">Data Services</p>
+          <div className="flex flex-wrap justify-around">
             <p className="flex px-4 py-2 text-slate-500">
-              <CloudUploadIcon className="h-6 text-indigo-600" /> App Security
+              <CloudUploadIcon className="h-6 text-indigo-600 mr-2" /> App
+              Security
             </p>
             <p className="flex px-4 py-2 text-slate-500">
-              <DatabaseIcon className="h-6 text-indigo-600" /> Dashboard Design
+              <DatabaseIcon className="h-6 text-indigo-600 mr-2" /> Dashboard
+              Design
             </p>
             <p className="flex px-4 py-2 text-slate-500">
-              <ServerIcon className="h-6 text-indigo-600" /> Cloud Data
+              <ServerIcon className="h-6 text-indigo-600 mr-2" /> Cloud Data
             </p>
             <p className="flex px-4 py-2 text-slate-500">
-              <PaperAirplaneIcon className="h-6 text-indigo-600" /> API
+              <PaperAirplaneIcon className="h-6 text-indigo-600 mr-2" /> API
             </p>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   const handleClose = () => setNav(!nav);
 
   return (
-    <div className="w-screen h-[80px] bg-zinc-200 fixed drop-shadow-lg ">
+    <div className="w-screen h-[80px] bg-zinc-200 fixed drop-shadow-lg z-50">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND</h1>
